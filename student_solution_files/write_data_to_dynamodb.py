7e0ece309feb1ec621662dfe88ea7eba88d9b354
Tholumuzi Mvubu <mvubutholumuzi@gmail.com>
2022-11-03 11:01:52 +0200

    new add

--- student_solution_files/write_data_to_dynamodb.py
+++ student_solution_files/write_data_to_dynamodb.py
@@ -55,7 +55,7 @@ def lambda_handler(event, context):
     # Complete the below code so that the appropriate 
     # incoming data is sent to the matching column in your DynamoDB table
     # --- Insert your code here ---
-    db_response = table.put_item(Item={'ResponsesID':10, # <--- Insert the correct variable
+    db_response = table.put_item(Item={'ResponsesID':rid, # <--- Insert the correct variable
                         'Name': 'Tholumuzi', # <--- Insert the correct variable
                         'Email': 'mvubutholumuzi@gmail.com', # <--- Insert the correct variable
                         'Cell': 824783592, # <--- Insert the correct variable
